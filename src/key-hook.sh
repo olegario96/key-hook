5fe314b287874569cd70843d259a28839a9eda52
Gustavo Olegario <gustavo-olegario@hotmail.com>
2019-10-21 08:32:07 -0300

    feat(src): Create main function using notify-send API

--- src/key-hook.sh
+++ src/key-hook.sh
@@ -10,13 +10,37 @@ get_keylock_status()
   set -- "$xset_";
   IFS=":"; declare -a Keylock=($*);
   readonly key="$(echo -e "${Keylock[$i]}" | printf '%s\n' "${Keylock[$i]//[[:digit:]]/}" | tr -d '[:space:]' )";
-  eval $key;
   echo "$key";
 }
 
 main()
 {
-    get_keylock_status 2 caps;
-    echo $caps;
+  caps=$(get_keylock_status 2);
+  num=$(get_keylock_status 4);
+  scroll=$(get_keylock_status 6);
+
+  while :
+  do
+    caps_=$(get_keylock_status 2);
+    num_=$(get_keylock_status 4);
+    scroll_=$(get_keylock_status 6);
+
+    if [ "$caps" != "$caps_" ]; then
+      notify-send "Key hook" "CapsLock is ${caps_}";
+      caps=$caps_
+    fi
+
+    if [ "$num" != "$num_" ]; then
+      notify-send "Key hook" "NumLock is ${num_}";
+      num=$num_
+    fi
+
+    if [ "$scroll" != "$scroll_" ]; then
+      notify-send "Key hook" "ScrollLock is ${scroll_}";
+      scroll=$scroll_
+    fi
+  done
 }
+
+main;
 exit 0;
